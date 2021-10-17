1bafca84c315adf584841f8e2b7e96e3906a6483
Egor <egduraev1997@gmail.com>
2021-10-17 21:49:58 +0300

    Homework 9

--- Task 1-2.c
+++ Task 1-2.c
@@ -1,139 +1,178 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define true 1 == 1
+#define false 1 != 1
+#define SZ 10
+#define SIZE 1000
+#define true 1 == 1
+#define false 1 != 1
 
-//TASK 1:
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+
+typedef struct {
+    int pr;
+    int dat;
+} Node;
+
+Node* arr[SZ];
+int head;
+int tail;
+int items;
+
+void init() {
+    for (int i = 0; i < SZ; ++i) {
+        arr[i] = NULL;
+    }
+    head = 0;
+    tail = 0;
+    items = 0;
 }
 
-void fillRandom(int* arr, int len, int border) {
-    for (int i = 0; i < len; ++i) {
-        *(arr + i) = rand() % border;
-    }
-}
+void ins (int pr, int dat) {
+    Node *node = (Node*) malloc (sizeof(Node));
+    node -> dat = dat;
+    node -> pr = pr;
 
-void printArray(int* arr, int len) {
-    for (int i = 0; i < len; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-void sortInserts(int arr[], int low, int n) {
-    for(int i = low + 1; i < n + 1; i++) {
-        int val = arr[i] ;
-        int j = i ;
-        while (j > low && arr[j-1] > val) {
-            arr[j] = arr[j-1] ;
-            j -= 1;
-        }
-        arr[j] = val ;
-    }
-}
-
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high] ;
-    int i ,j;
-    i = low;
-    j = low;
-    for (int i = low; i < high; i++) {
-        if(arr[i] < pivot) {
-            swap(&arr[i], &arr[j]);
-            j += 1;
-        }
-    }
-    swap(&arr[j], &arr[high]);
-    return j;
-}
-
-void qs(int arr[], int low,int high) {
-    if (low < high) {
-        int pivot = partition(arr, low, high);
-        qs(arr, low, pivot-1) ;
-        qs(arr, pivot + 1, high) ;
-    }
-}
-
-void qsAdvanced(int arr[], int low, int high) {
-    while (low < high) {
-        if (high - low + 1 < 10)
-        {
-            sortInserts(arr, low, high);
-            break;
-        }
-        else
-        {
-            int pivot = partition(arr, low, high) ;
-            if (pivot - low < high - pivot)
-            {
-                qsAdvanced(arr, low, pivot - 1);
-                low = pivot + 1;
-            }
-            else
-            {
-                qsAdvanced(arr, pivot + 1, high);
-                high = pivot-1;
-            }
-        }
-    }
-}
-
-// TASK 2:
-
-void bucketSort(int* arr, int len) {
-    const int max = len;
-    const int b = 10;
-
-    int buckets[b][max + 1];
-    for (int i = 0; i < b; ++i) {
-        buckets[i][max] = 0;
-    }
-
-    for (int digit = 1; digit < 1000000000; digit *= 10) {
-        for (int i = 0; i < max; ++i) {
-            if ((arr[i] % 2) != 0) continue;
-            int d = (arr[i] / digit) % b;
-            int counter = buckets[d][max];
-            buckets[d][counter] = arr[i];
-            counter++;
-            buckets[d][max] = counter;
-        }
+    if (items == 0) {
+        arr[tail++] = node;
+        items++;
+    } else if(items == SZ) {
+        printf("%s \n", "Queue is full");
+        return;
+    } else {
         int idx = 0;
-        for (int i = 0; i < b; ++i) {
-            for (int j = 0; j < buckets[i][max]; ++j) {
-                while ((arr[idx] % 2) != 0) {
-                    idx++;
-                }
-                if (buckets[b][max] == 0) continue;
-                arr[idx] = buckets[i][j];
-                idx++;
+        idx = tail % SZ;
+        arr[idx] = node;
+        items++;
+        tail++;
+    }
+}
 
+
+Node* rem() {
+    if (items == 0) {
+        return 0;
+    } else {
+        int idx = 0;
+        int idx1 = 0;
+        int pr = arr[head] -> pr;
+        for ( int i = head; i < tail; ++i) {
+            idx1 = i % SZ;
+            if (pr > (arr[idx1] -> pr)) {
+                pr = arr[idx1] -> pr;
             }
-            buckets[i][max] = 0;
+
+        }
+        int i = 0;
+        for (i = head; i < tail; ++i) {
+            idx = i % SZ;
+            if ((arr[idx] -> pr) == pr)
+                break;
+        }
+        if (i == head) {
+            idx = head++ % SZ;
+            Node *tmp = arr[idx];
+            arr[idx] = NULL;
+            items--;
+            return tmp;
+        } else {
+            int idxz = 0;
+            Node *tmp = arr[idx];
+            i--;
+            while (i >= head) {
+                idx = i % SZ;
+                idxz = (i + 1) % SZ;
+                arr[idxz] = arr[idx];
+                i--;
+            }
+            arr[idx] = NULL;
+            items--;
+            head++;
+            return tmp;
         }
     }
 }
 
-int main() {
-    const int SZ = 30;
-    int arr[SZ];
-    printf("TASK 1 \n");
-    fillRandom(arr, SZ, 100);
-    printArray(arr, SZ);
-    qs(arr, 0, SZ);
-    printf("Hoare quicksort: \n");
-    printArray(arr, SZ);
-    qsAdvanced(arr, 0, SZ);
-    printf("Advanced quicksort: \n");
-    printArray(arr, SZ);
-    printf("\n\n");
-    printf("TASK 2 \n");
-    fillRandom(arr, SZ, 30);
-    printArray(arr, SZ);
-    bucketSort(arr, SZ);
-    printArray(arr, SZ);
-
+void printQueue() {
+    printf("[ ");
+    for (int i = 0; i < SZ; ++i) {
+        if (arr[i] == NULL)
+            printf("[* , *}");
+        else
+            printf("[%d, %d]", arr[i] -> pr, arr[i] -> dat);
+    }
+    printf(" ]\n");
 }
 
+
+int cursor = -1;
+int Stack[SIZE];
+int push(int data) {
+    if (cursor < SIZE) {
+        Stack[++cursor] = data;
+        return true;
+    } else {
+        printf("%s \n", "Stack overflow");
+        return false;
+    }
+}
+
+int pop() {
+    if (cursor != -1) {
+        return Stack[cursor--];
+    } else {
+        printf("%s \n", "Stack is empty");
+        return -1;
+    }
+}
+
+void ToBin(int num) {
+    while(num != 0) {
+        push(num % 2);
+        num = num / 2;
+    }
+}
+
+int main()
+{
+    printf("TASK 1: \n");
+    init();
+    ins(1, 11);
+    ins(3, 22);
+    ins(4, 33);
+    ins(2, 44);
+    ins(3, 55);
+    ins(4, 66);
+    ins(5, 77);
+    ins(5, 88);
+    ins(1, 99);
+    ins(6, 100);
+    printQueue();
+    for (int i = 0; i < 3; ++i) {
+        Node* n = rem();
+        printf("pr = %d, dat = %d \n", n -> pr, n -> dat);
+    }
+    printQueue();
+    ins(1, 110);
+    ins(3, 120);
+    ins(6, 130);
+    printQueue();
+    for (int i = 0; i < 5; ++i) {
+        Node* n = rem();
+        printf("pr = %d, dat = %d \n", n -> pr, n -> dat);
+    }
+    printQueue();
+
+    ins(2, 140);
+    ins(4, 150);
+    printQueue();
+
+    printf("TASK 2: \n");
+    int num;
+    printf("Please, enter the number: ");
+    scanf("%d", &num);
+    ToBin(num);
+    while (cursor != -1) {
+        printf("%d ", pop());
+    }
+    return 0;
+}
